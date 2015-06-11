47beb07a4fd4965b77978954f69b0d065d1bfeff
ccslakey <cromwellslakey@gmail.com>
2015-06-11 08:59:10 -0700

    Raised book model to class variable for rendering. Still need to render libs, auths, and CRUD options for user

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+  	# raising @books to class variable for rendering??
+  	@books = Book.all
+  	@book = Book.new
+  	render :index
   end
 end
